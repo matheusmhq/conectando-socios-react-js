a03be15404e9725e5948b09776f7a5ae4edccc76
Matheus <mhqsoftware@gmail.com>
2021-07-25 17:49:01 -0300

    Remove import

--- src/components/Cards/CardProject.js
+++ src/components/Cards/CardProject.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { Card, Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMapMarker,
